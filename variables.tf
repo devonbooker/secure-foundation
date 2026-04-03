7662f49d5f5aefcdcd3b68ed06788dc85ff016e0
Devon Booker <devonbookerofficial@gmail.com>
2026-04-03 16:56:39 -0500

    add VPC with public and private subnets

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "project_name" {
+  description = "Project name used for tagging"
+  type        = string
+  default     = "secure-foundation"
+}
